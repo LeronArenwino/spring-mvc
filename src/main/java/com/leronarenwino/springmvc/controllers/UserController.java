edde17baf0f5ab62f53adf99cf7dc878dc3b48d0
Francisco Dueñas <frsduenasca@unal.edu.co>
2024-01-13 23:45:15 -0500

    :sparkles: [UPDATE] Using Model

--- src/main/java/com/leronarenwino/springmvc/controllers/UserController.java
+++ src/main/java/com/leronarenwino/springmvc/controllers/UserController.java
@@ -1,13 +1,18 @@
 package com.leronarenwino.springmvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details(){
+    public String details(Model model){
+
+        model.addAttribute("title", "Hello World");
+        model.addAttribute("name", "Leron");
+        model.addAttribute("lastname", "Arenwino");
 
         return "details";
 
