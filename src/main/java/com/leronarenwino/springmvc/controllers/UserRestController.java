51b9ad702d44445ee2f77e790496bfc414383d23
Francisco Dueñas <frsduenasca@unal.edu.co>
2024-01-14 19:01:17 -0500

    :sparkles: [UPDATE] Using RequestMapping annotation

--- src/main/java/com/leronarenwino/springmvc/controllers/UserRestController.java
+++ src/main/java/com/leronarenwino/springmvc/controllers/UserRestController.java
@@ -1,15 +1,17 @@
 package com.leronarenwino.springmvc.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
 
-    @GetMapping("/details2")
+    @GetMapping("/details")
     public Map<String, Object> details(){
 
         Map<String, Object> body = new HashMap<>();
