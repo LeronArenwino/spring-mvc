15fc6e7f9429e474dedcdaaa1f64dd06d7af6c29
Francisco Dueñas <frsduenasca@unal.edu.co>
2024-01-19 21:56:17 -0500

    :sparkles: [UPDATE] Using DTO

--- src/main/java/com/leronarenwino/springmvc/controllers/UserRestController.java
+++ src/main/java/com/leronarenwino/springmvc/controllers/UserRestController.java
@@ -1,29 +1,25 @@
 package com.leronarenwino.springmvc.controllers;
 
 import com.leronarenwino.springmvc.models.User;
+import com.leronarenwino.springmvc.models.dto.UserDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
     @GetMapping("/details")
-    public Map<String, Object> details(){
+    public UserDto details(){
 
         User user = new User("Leron", "Arenwino");
 
-        Map<String, Object> body = new HashMap<>();
+        UserDto userDto = new UserDto();
+        userDto.setUser(user);
+        userDto.setTitle("Hello World");
 
-        body.put("title", "Hello World");
-        body.put("user", user);
-
-        return body;
-
+        return userDto;
     }
 
 }
