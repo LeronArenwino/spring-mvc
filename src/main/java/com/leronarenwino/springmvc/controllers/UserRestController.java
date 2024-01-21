3a9edf5804aefca107587cd2d579d9c9c2b43bd1
Francisco Dueñas <frsduenasca@unal.edu.co>
2024-01-20 20:20:27 -0500

    :sparkles: [UPDATE] New endpoint list

--- src/main/java/com/leronarenwino/springmvc/controllers/UserRestController.java
+++ src/main/java/com/leronarenwino/springmvc/controllers/UserRestController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
@@ -20,6 +23,16 @@ public class UserRestController {
         userDto.setTitle("Hello World");
 
         return userDto;
+    }
+
+    @GetMapping("/list")
+    public List<User> list(){
+
+        User user = new User("Andres", "Guzman");
+        User user2 = new User("Harry", "Doe");
+        User user3 = new User("Warm", "Doe");
+
+        return Arrays.asList(user, user2, user3);
     }
 
 }
