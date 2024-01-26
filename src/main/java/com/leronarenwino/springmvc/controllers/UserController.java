b5a557b7a5a7d240e98d6a15eacb472882d3b7be
Francisco Dueñas <frsduenasca@unal.edu.co>
2024-01-25 21:13:25 -0500

    :sparkles: [UPDATE] List view and mapping

--- src/main/java/com/leronarenwino/springmvc/controllers/UserController.java
+++ src/main/java/com/leronarenwino/springmvc/controllers/UserController.java
@@ -3,7 +3,11 @@ package com.leronarenwino.springmvc.controllers;
 import com.leronarenwino.springmvc.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -20,5 +24,15 @@ public class UserController {
         return "details";
 
     }
+
+    @GetMapping("/list")
+    public String list(ModelMap model){
+        List<User> userList = new ArrayList<>();
+
+        model.addAttribute("title", "Lista de usuarios");
+        model.addAttribute("users", userList);
+        return "list";
+    }
+
 
 }
